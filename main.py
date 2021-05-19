a5c1c319ec476e9da276ac276eae2312db03aa54
Peeush Agarwal <agarwal.peeush.developer@gmail.com>
2021-05-19 22:16:52 +0530

    Update logic to handle order of mch and cac

--- main.py
+++ main.py
@@ -62,37 +62,13 @@ class Store:
             'nsh': Item('nsh', 'Nike Shoe', 109.50),
             'mch': Item('mch', 'Charger', 30.00),
         }
-        self.pricing_rules_dict = {}
         print('Store created')
 
-    def display_item(self, item_id: str, prefix: str = '') -> None:
-        item = self.items_dict[item_id]
-        print(f'{prefix} : {item.name:<10} | {item_id}'.strip())
+    def get_item(self, item_id: str) -> Item:
+        return self.items_dict[item_id]
 
-    def remove_item(self, item_id: str, quantity: int) -> None:
-        print('Store remove_item get called')
-        pass
-
-    def compute_amount(self, item_id: str, quantity: int) -> float:
-        # print(item_id)
-        price_per_item = self.items_dict[item_id].price
-        if item_id in self.pricing_rules_dict:
-            pricing_rule = self.pricing_rules_dict[item_id]
-            # print(type(pricing_rule))
-            if not isinstance(pricing_rule, PriceRule_BuyThis_GetThatFree):
-                return pricing_rule.compute_amount(price_per_item, quantity)
-            else:
-                # print('PriceRule_BuyThis_GetThatFree detected')
-                return self.compute_amount_buy_this_get_that_free(price_per_item, quantity, pricing_rule)
-
-        return price_per_item * quantity
-
-    def compute_amount_buy_this_get_that_free(self,
-                                              price_per_item: float,
-                                              quantity: int,
-                                              pricing_rule: PriceRule_BuyThis_GetThatFree) -> float:
-        self.remove_item(pricing_rule.free_item_id, quantity)
-        return pricing_rule.compute_amount(price_per_item, quantity)
+    def get_price_per_item(self, item_id: str) -> float:
+        return self.get_item(item_id).price
 
 
 class Checkout(Store):
@@ -100,30 +76,61 @@ class Checkout(Store):
 
     def __init__(self, pricing_rules: Pricing_Rules = []) -> None:
         super().__init__()
-        self.pricing_rules_dict = {
+        self.__pricing_rules_dict__ = {
             pricing_rule.item_id: pricing_rule for pricing_rule in pricing_rules}
-        self.item_id_count = {}
+        self.__item_id_count__ = {}
+        self.__item_processed__ = {}
         print('Checkout created')
 
     def scan(self, item_id: str) -> None:
-        if item_id not in self.item_id_count:
-            self.item_id_count[item_id] = 0
-        self.item_id_count[item_id] += 1
-        self.display_item(item_id, prefix='Scanned')
-
-    def remove_item(self, item_id: str, quantity: int) -> None:
-        # print('Checkout remove_item get called')
-        if item_id in self.item_id_count:
-            count = self.item_id_count[item_id]
-            self.item_id_count[item_id] = max(0, count - quantity)
-        else:
-            super().remove_item(item_id, quantity)
+        if item_id not in self.__item_id_count__:
+            self.__item_id_count__[item_id] = 0
+        self.__item_id_count__[item_id] += 1
+        self.__display_item__(item_id, prefix='Scanned')
 
     def total(self) -> None:
-        total = 0.0
-        for item_id, quantity in self.item_id_count.items():
-            total += self.compute_amount(item_id, quantity)
-        print('Total: ${0:.2f}'.format(total))
+        self.__total__ = 0.0
+        for item_id, quantity in self.__item_id_count__.items():
+            amount = self.__compute_amount__(item_id, quantity)
+            self.__total__ += amount
+            self.__item_processed__[item_id] = quantity
+        print('Total: ${0:.2f}'.format(self.__total__))
+
+    def __compute_amount__(self, item_id: str, quantity: int) -> float:
+        price_per_item = self.get_price_per_item(item_id)
+        if item_id in self.__pricing_rules_dict__:
+            pricing_rule = self.__pricing_rules_dict__[item_id]
+            if not isinstance(pricing_rule, PriceRule_BuyThis_GetThatFree):
+                return pricing_rule.compute_amount(price_per_item, quantity)
+            else:
+                return self.__compute_amount_buy_this_get_that_free__(price_per_item, quantity, pricing_rule)
+
+        return price_per_item * quantity
+
+    def __compute_amount_buy_this_get_that_free__(self,
+                                                  price_per_item: float,
+                                                  quantity: int,
+                                                  pricing_rule: PriceRule_BuyThis_GetThatFree) -> float:
+        self.__remove_item__(pricing_rule.free_item_id, quantity)
+        return pricing_rule.compute_amount(price_per_item, quantity)
+
+    def __display_item__(self, item_id: str, prefix: str = '') -> None:
+        item = self.get_item(item_id)
+        print(f'{prefix} : {item.name:<10} | {item_id}'.strip())
+
+    def __remove_item__(self, item_id: str, quantity: int) -> None:
+        if item_id in self.__item_id_count__:
+            existing_quantity = self.__item_id_count__[item_id]
+            self.__item_id_count__[item_id] = max(
+                0, existing_quantity - quantity)
+        # Handle already processed items
+        if item_id in self.__item_processed__:
+            processed_quantity = self.__item_processed__[item_id]
+            price_per_item = self.get_price_per_item(item_id)
+            if processed_quantity <= quantity:
+                self.__total__ -= processed_quantity * price_per_item
+            else:
+                self.__total__ -= quantity * price_per_item
 
 
 if __name__ == '__main__':
